6e51d7e91976148fb4c56ebf1cfaf95da6e08969
Nathalie Fajardo <nathaliefc89@gmail.com>
2024-02-05 17:23:54 +0000

    Adding custom http loading & error states

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,28 +1,22 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from './hooks/useHttp.js';
+
+const requestConfig = {};
 
 export default function Meals() {
-  const [lodedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      
-      if (!response.ok) {
-        //..
-      }
-
-      const meals = await response.json();
-      
-      setLoadedMeals(meals);
-    }
-    
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>
+  }
 
   return (
     <ul id="meals">
-      {lodedMeals.map((meal) => (
+      {loadedMeals.map((meal) => (
         <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
